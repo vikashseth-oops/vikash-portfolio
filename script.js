3cdc4a10f4df5178710adadbaaa46e90197c543f
vikash <sethv4506@gmail.com>
2026-01-30 01:31:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+new Typed('#element', {
+    strings: [
+        'Web Developer',
+        'Software Developer',
+        'Problem Solver',
+        'Frontend Developer'
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
